2d73f151feed4891a0f78a626fddb1f2335317de
shivek.sachdev <shivek@tis.co.th>
2023-05-14 19:45:16 +0700

    clinic version released with Kana products

---
+++ utils_clinic.py
@@ -0,0 +1,21 @@
+import openai
+
+def get_initial_message(persona):
+    messages=[
+            {"role": "system", "content": persona},
+            {"role": "user", "content": "Hey there!"},
+            {"role": "assistant", "content": "Welcome to our dispensary!"}
+        ]
+    return messages
+
+def get_chatgpt_response(messages, model="gpt-4"):
+    print("model: ", model)
+    response = openai.ChatCompletion.create(
+    model=model,
+    messages=messages
+    )
+    return  response['choices'][0]['message']['content']
+
+def update_chat(messages, role, content):
+    messages.append({"role": role, "content": content})
+    return messages
